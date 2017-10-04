3d26416e4e35de93b8a266ce4718082999e49f5f
Coul33t <Coulis1990@gmail.com>
2017-10-04 16:41:33 +0200

    Added the possibility to select only certain joints, plus some comments

--- data_visualization.py
+++ data_visualization.py
@@ -69,9 +69,12 @@ def motion_dict_to_list(data_dict):
 def data_gathering_dict(folder_path, joints_to_append=None):
     """
         Put the data from multiples files (in the folder folder_path) into a dictionnary.
+        joints_to_append is there if you want only certains joints.
     """
     file_list = file_name_gathering(folder_path)
+
     # Oh my god I'm so ashamed it took me so long to find this ...
+    # Sort the files into natural order (instead of string order)
     file_list.sort(key=natural_keys)
 
     data = OrderedDict()
@@ -90,11 +93,14 @@ def data_gathering_dict(folder_path, joints_to_append=None):
                 # if there's actually something (IF not_empty AND first_value AND second_value)
                 if splitted_line and splitted_line[0] and splitted_line[1]:
 
-                    # setdefault() append to they value if the key exists, else it adds the key and append
-                    if 'ang' in filename:
-                        data.setdefault(splitted_line[0], []).append(math.degrees(float(splitted_line[1])))
-                    else:
-                        data.setdefault(splitted_line[0], []).append(float(splitted_line[1]))
+                    # If there's no joints_to_append parameter, then do it all time
+                    # If there's a joints_to_append AND the name of the joint is in joints_to_append, do it
+                    if (not joints_to_append) or (joints_to_append and splitted_line[0] in joints_to_append):
+                        # setdefault() append to they value if the key exists, else it adds the key and append
+                        if 'ang' in filename:
+                            data.setdefault(splitted_line[0], []).append(math.degrees(float(splitted_line[1])))
+                        else:
+                            data.setdefault(splitted_line[0], []).append(float(splitted_line[1]))
                     
 
     return data
