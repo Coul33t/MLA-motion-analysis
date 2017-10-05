629e8b4ab57917dc297427184a7b8f5d32795259
Coul33t <Coulis1990@gmail.com>
2017-10-05 12:21:43 +0200

    Further separation

--- data_visualization.py
+++ data_visualization.py
@@ -2,7 +2,6 @@ import math
 import pdb
 
 import os
-from tools import flatten_list, motion_dict_to_list
 
 import numpy as np
 import matplotlib.patches as mpatches
@@ -11,18 +10,9 @@ import matplotlib.cm as cm
 
 from scipy.signal import savgol_filter, correlate
 
-from data_import import *
-
-from kmeans_algo import *
-from affinity_propagation_algo import *
-from mean_shift_algo import *
-
-#TODO : split visualization and data processing
+from data_import import data_gathering_dict
 #TODO : better data visualization function
-def bvh_parser(folder):
-    pass 
     
-
 
 def visualization(motion_type="gimbal"):
     folder_path_lin = r'C:\Users\quentin\Documents\Programmation\C++\MLA\Data\Speed\throw_5_gimbal_smooth_16\lin'
@@ -127,95 +117,3 @@ def multifiles_visualization(motion_type="gimbal"):
         ls.plot(x, ysavgol, linestyle='solid', color=c)
 
     plt.show()
-
-def test_mean_speed_intervals(motion_type="gimbal"):
-    folder = r'C:\Users\quentin\Documents\Programmation\C++\MLA\Data\Speed'
-
-    data_lin = []
-    data_lin_2 = []
-
-    subdirectories = os.listdir(folder)
-    subdirectories.sort(key=natural_keys)
-
-    for name in subdirectories:
-        if motion_type in name:
-            print("Appending {}".format(name))
-            data_lin.append(flatten_list(motion_dict_to_list(data_gathering_dict(folder+'\\'+name+'\\lin_mean_10_cut'))))
-
-    pdb.set_trace()
-    kmeans_algo(data_lin)
-
-def test_mean_speed_intervals_batch(size, motion_type='gimbal'):
-    folder = r'C:\Users\quentin\Documents\Programmation\C++\MLA\Data\Speed'
-
-    subdirectories = os.listdir(folder)
-    subdirectories.sort(key=natural_keys)
-
-    data_lin = [[] for x in range(size)]
-    names = []
-
-    # For each folder
-    for name in subdirectories:
-
-        # If the folder's name contain the name of the motion
-        if motion_type in name:
-
-            print(name)
-            subsubdirectories = os.listdir(folder+'\\'+name)
-            subsubdirectories.sort(key=natural_keys)
-
-            i = 0
-            # For each file in the folder
-            for subname in subsubdirectories:
-                if 'lin_mean' in subname:
-                    
-                    if subname not in names:
-                        names.append(subname)
-
-                    print(subname)
-
-                    # Append data
-                    data_lin[i].append(flatten_list(motion_dict_to_list(data_gathering_dict(folder+'\\'+name+'\\' + subname))))
-                    i += 1
-
-    res = []
-
-    # Actual ML
-
-    for i, different_cut in enumerate(data_lin):
-        print('Batch : {}'.format(i))
-        res.append(kmeans_algo(different_cut))
-        #res.append(affinity_propagation_algo(different_cut))
-        #res.append(mean_shift_algo(different_cut))
-        
-
-    #display_res(res, names)
-    pdb.set_trace()
-
-def display_res(res, names):
-    for i,batch in enumerate(res):
-        print('\n-------------------------------')
-        print('New batch : {}'.format(names[i]))
-        print('-------------------------------')
-        for algo in batch:
-            pdb.set_trace()
-            print('{} : [min: {}] [max: {}] [mean: {}]'.format(algo[0], min(algo[1]), max(algo[1]), sum(algo[1])/len(algo[1])))
-
-
-def all_functions():
-    # visualization()
-    # multifiles_visualization()
-    # mean_speed()
-    
-    # # Python can't lenny face :(
-    # print('( ͡° ͜ʖ ͡°)')
-    # test_normalization()
-    # test_dtw()
-    # kmeans_algo(new_data)
-    # ml()
-    # test_mean_speed_intervals("cut")
-    #test_mean_speed_intervals_batch(19, motion_type='cut')
-    pass
-
-if __name__ == '__main__':
-    all_functions()
--- main.py
+++ main.py
@@ -1,8 +1,99 @@
+import os
+import pdb
+
 from data_visualization import multifiles_visualization
+from tools import flatten_list, motion_dict_to_list, natural_keys
+from data_import import data_gathering_dict
+from algo.kmeans_algo import kmeans_algo
+
+def test_mean_speed_intervals(motion_type="gimbal"):
+    folder = r'C:\Users\quentin\Documents\Programmation\C++\MLA\Data\Speed'
+
+    data_lin = []
+    data_lin_2 = []
+
+    subdirectories = os.listdir(folder)
+    subdirectories.sort(key=natural_keys)
+
+    for name in subdirectories:
+        if motion_type in name:
+            print("Appending {}".format(name))
+            data_lin.append(flatten_list(motion_dict_to_list(data_gathering_dict(folder+'\\'+name+'\\lin_mean_10_cut'))))
+
+    pdb.set_trace()
+    kmeans_algo(data_lin)
+
+
+
+
+
+def test_mean_speed_intervals_batch(size, motion_type='gimbal'):
+    folder = r'C:\Users\quentin\Documents\Programmation\C++\MLA\Data\Speed'
+
+    subdirectories = os.listdir(folder)
+    subdirectories.sort(key=natural_keys)
+
+    data_lin = [[] for x in range(size)]
+    names = []
+
+    # For each folder
+    for name in subdirectories:
+
+        # If the folder's name contain the name of the motion
+        if motion_type in name:
+
+            print(name)
+            subsubdirectories = os.listdir(folder+'\\'+name)
+            subsubdirectories.sort(key=natural_keys)
+
+            i = 0
+            # For each file in the folder
+            for subname in subsubdirectories:
+                if 'lin_mean' in subname:
+                    
+                    if subname not in names:
+                        names.append(subname)
+
+                    print(subname)
+
+                    # Append data
+                    data_lin[i].append(flatten_list(motion_dict_to_list(data_gathering_dict(folder+'\\'+name+'\\' + subname))))
+                    i += 1
+
+    res = []
+
+    # Actual ML
+
+    for i, different_cut in enumerate(data_lin):
+        print('Batch : {}'.format(i))
+        res.append(kmeans_algo(different_cut))
+        #res.append(affinity_propagation_algo(different_cut))
+        #res.append(mean_shift_algo(different_cut))
+        
+
+    #display_res(res, names)
+    pdb.set_trace()
+
+
+
+
+
+def display_res(res, names):
+    for i,batch in enumerate(res):
+        print('\n-------------------------------')
+        print('New batch : {}'.format(names[i]))
+        print('-------------------------------')
+        for algo in batch:
+            pdb.set_trace()
+            print('{} : [min: {}] [max: {}] [mean: {}]'.format(algo[0], min(algo[1]), max(algo[1]), sum(algo[1])/len(algo[1])))
+
+
+
+
 
 def main():
     # visualization()
-    multifiles_visualization()
+    # multifiles_visualization()
     # mean_speed()
     
     # # Python can't lenny face :(
@@ -13,7 +104,7 @@ def main():
     # kmeans_algo(new_data)
     # ml()
     # test_mean_speed_intervals("cut")
-    #test_mean_speed_intervals_batch(19, motion_type='cut')
+    test_mean_speed_intervals_batch(19, motion_type='cut')
 
 if __name__ == '__main__':
     main()
