8cc652be6fc34eb86a7eb81cab98ae7e14032b3d
Coul33t <Coulis1990@gmail.com>
2018-01-24 15:06:38 +0100

    Added an extreme_verbose parameters (output at each algorithm's iteration)

--- algos/kmeans_algo.py
+++ algos/kmeans_algo.py
@@ -1,10 +1,11 @@
 from sklearn.cluster import KMeans
 
-def kmeans_algo(data, algo='k-means++', verbose=False):
+def kmeans_algo(data, algo='k-means++', verbose=False, extrem_verbose=False):
 
     if verbose:
         print("\n\n{}\n".format(algo))
 
-    res = KMeans(2, algo, n_init=20, max_iter=1000).fit(list(data))
+    # About fit vs fit_predict: https://stackoverflow.com/questions/25012342/scikit-learns-k-means-what-does-the-predict-method-really-do
+    res = KMeans(2, algo, n_init=20, max_iter=10000, verbose=extrem_verbose).fit(list(data))
 
     return res
