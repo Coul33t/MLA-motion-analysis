af6609bd218af0597e8f31c5fd8c24e0390d4a7a
Coul33t <Coulis1990@gmail.com>
2018-01-24 15:07:38 +0100

    Splitted functions, started using Numpy's arrays instead of lists

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
+# Native packages
 import os
+from collections import OrderedDict
 import statistics as stat
-
 import pdb
 
+# External lib packages
+import numpy as np
 from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
-from data_visualization import multifiles_visualization
+# Personnal packages
 from tools import flatten_list, motion_dict_to_list, natural_keys, select_joint
 from data_import import data_gathering_dict, return_data, adhoc_gathering, json_import
 from algos.kmeans_algo import kmeans_algo
 from data_visualization import visualization, plot_2d
 from data_processing import delta_computing
-
-import matplotlib.pyplot as plt
 
 
 def test_mean_speed_intervals(motion_type="gimbal", joints_to_append=None):
@@ -85,91 +87,140 @@ def test_mean_speed_intervals_batch(size, motion_type='gimbal', joints_to_append
 
 
 
-def test_full_batch(path, joints_to_append=None, transform=None):
-    original_data = json_import(path)
-    # adhoc because for the moment, we have multiple segments
-    original_data = adhoc_gathering(path, joints_to_append)
-
-    pdb.set_trace()
+def joint_selection(data, joints_to_append):
+    
+    # Joint selection
+    # List ( List (motion) [
+    #                       str (name), dict (datatype) [
+    #                                                    k:str (datatype name), v:dict (joints) [
+    #                                                                                            k: str (joint), v:liss[double] (values)
+    #                                                                                           ]
+    #                                                   ]
+    #                      ]
+    # )
+    # We gonna switch from dict to OrederedDict, to ensure that each
+    # features vector has the same joints order
     selected_data = []
 
-    for motion in original_data:
-        selected_data.append(list(motion.values()))
+    datatypes = list(data[0][1].keys())
+    # If joints_to_append isn't specified, we take all the joints.
+    # Just to be sure that the order won't change between 2 motion dicts, 
+    # we take the keys of one motion and use it as the reference for insertion order
+    if not joints_to_append:
+        joints_to_append = list(data[0][1][datatypes[0]].keys())
 
-    if not transform or transform == 'meanmax':
-    # ----- MEAN AND MAX ------
-    # Here, we transform the features into mean and max speed (2 features)
-        print("Min + max")
-        features = []
-        for motion in selected_data:
-            feat_to_add = []
-            for joint in motion:
-                feat_to_add.append([stat.mean(joint), max(joint)])
-            features.append(flatten_list(feat_to_add))
+    for motion in data:
+        # We keep the name
+        selected_joints_motion = [motion[0], OrderedDict()]
 
-        for i in range(20):
-            res = kmeans_algo(features)
-            print('{}'.format(LOUP_rate(res.labels_)))
+        # For each datatype
+        for datatype in datatypes:
+            
+            # We use an OrderedDict. Since we iterate over the joints_to_append list,
+            # the order is ensured to be the same for each motion
+            joints_selected = OrderedDict()
 
-    # ----- MEAN AND MAX ------
+            # For each joint to append
+            for joint in joints_to_append:
+                joints_selected[joint] = motion[1][datatype][joint]
 
-    if not transform or transform == 'delta':
-    # ----- DELTA COMPUTING -----
-    # Here, we compute the difference between subsequent velocities
-        print("Delta speed")
-        features = []
-        for motion in selected_data:
-            feat_to_add = []
-            for joint in motion:
-                for i, _ in enumerate(joint):
-                    if i > 0:
-                        feat_to_add.append(abs(joint[i-1] - joint[i]))
+            selected_joints_motion[1][datatype] = joints_selected
 
-            features.append(feat_to_add)
+        selected_data.append(selected_joints_motion)
 
-        for i in range(20):
-            res = kmeans_algo(features)
-            print('{}'.format(LOUP_rate(res.labels_)))
-    # ----- DELTA COMPUTING -----
+    return selected_data
 
-    if not transform or transform == 'full':
-    # ----- FULL DATA -----
-        print("Full")
-        features = []
-        for motion in selected_data:
-            features.append(flatten_list(motion))
 
-        for i in range(20):
-            res = kmeans_algo(features)
-            print('{}'.format(LOUP_rate(res.labels_)))
 
-    # ----- FULL DATA -----
 
-    # ----- PCA TESTS -----
-    if not transform or transform == 'pca':
-        print("PCA")
-        features = []
-        rates = []
-        for i in range(len(flatten_list(selected_data[0])) - 1):
 
-            print('{}/{} features'.format((i+1), len(flatten_list(selected_data[0])) - 1))
-            features = []
-            for motion in selected_data:
-                features.append(flatten_list(motion))
+def data_selection(data, data_to_keep):
 
-            pca = PCA(n_components=i+1)
-            pca.fit(features)
-            features = pca.transform(features)
+    features = []
 
-            res = kmeans_algo(features)
+    # For each motion
+    for motion in data:
+        motion_feat = OrderedDict()
+        
 
-            rates.append((LOUP_rate(res.labels_), i+1))
-            # print('nb features: {} / {}'.format(i+1, rate))
+        # Don't ask how it works (= nb_frames - 1 for speed)
+        nb_values = len(list(motion[1][list(motion[1].keys())[0]].values())[0])
 
-        print('Best: {} ({} features)'.format(max(rates, key=lambda item: item[0])[0], max(rates, key=lambda item: item[0])[1]))
-    # ----- PCA TESTS -----
-    # pdb.set_trace()
+        for i in range(nb_values):
+            # For each datatype
+            for datatype in motion[1]:
+                if datatype in data_to_keep:
+                # For each joint
+                    for joint in motion[1][datatype]:
+                        # If it doesn't exists, create an empty list then append
+                        # Else, append
+                        motion_feat.setdefault(joint, []).append(motion[1][datatype][joint][i])
 
+        features.append(flatten_list(list(motion_feat.values())))
+    
+    # We return the list as a numpy array, as it is more
+    # convenient to use
+    # TODO: change the code so it uses numpy array from the beginning
+    return np.asarray(features)
+
+
+
+
+
+def mean_matrix(data):
+    """
+        Compute the distance between each sample of the data
+    """
+    distance_matrix = np.zeros( (len(data), len(data)) )
+
+    for i in range(len(data)):
+        for j in range(len(data)):
+            distance_matrix[i][j] = abs(data[i] - data[j])
+
+    return distance_matrix
+
+
+
+
+def test_full_batch(path, joints_to_append=None):
+    original_data = json_import(path)
+
+    selected_data = joint_selection(original_data, joints_to_append)
+
+    data_to_select = ['Speed']
+    features = data_selection(selected_data, data_to_select)
+
+    res = kmeans_algo(features)
+    print('Good classification rate : {}\nInertia: {}'.format(LOUP_rate(res.labels_), res.inertia_))
+
+
+
+
+
+def test_full_batch_every_joint(path):
+    original_data = json_import(path)
+
+    joints_to_append = list(original_data[0][1]['Speed'].keys())
+
+    data_to_select = ['Speed', 'Speedx', 'Speedy', 'Speedz']
+
+    print('Data used: {}'.format(data_to_select))
+    str1 = 'Joint name'
+    str2 = 'Rate'
+    str3 = 'Inertia'
+    print(str1.ljust(30) + str2.ljust(15) + str3)
+    print('----------------------------------------------------------------')
+
+    for joint in joints_to_append:
+        selected_data = joint_selection(original_data, [joint])
+      
+        features = data_selection(selected_data, data_to_select)
+
+        res = kmeans_algo(features)
+        str1 = '{}'.format(joint)
+        str2 = '{}'.format(LOUP_rate(res.labels_))
+        str3 = '{}'.format(res.inertia_)
+        print(str1.ljust(30) + str2.ljust(15) + str3)
 
 def LOUP_rate(labels):
     true_labels = [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1]
@@ -185,29 +236,44 @@ def display_res(result_list):
         print('{} : [min: {}] [max: {}] [mean: {}]'.format(result[0], min(result[1]), max(result[1]), sum(result[1])/len(result[1])))
 
 
-
 def main():
-    # visualization(joints_to_visualize=['LeftShoulder', 'LeftForeArm', 'LeftArm', 'LeftHand'], savgol=True)
-    # visualization(joints_to_visualize=['LeftHand'], savgol=False)
-    # multifiles_visualization(joints_to_visualize=['Head', 'LeftShoulder', 'LeftForeArm', 'LeftArm', 'LeftHand'])
-    # mean_speed()
-
+    # test_full_batch(r'C:\Users\quentin\Documents\Programmation\C++\MLA\Data\Speed\\', joints_to_append=['Hips'])
     # # Python can't lenny face :(
     # print('( ͡° ͜ʖ ͡°)')
+    joints_to_test = [None,
+                      ['RightHand'],
+                      ['RightForeArm'],
+                      ['RightArm'],
+                      ['RightShoulder'],
+                      ['RightHand', 'RightArm'],
+                      ['RightHand', 'RightArm', 'RightForeArm'],
+                      ['RightHand', 'RightArm', 'RightForeArm', 'RightShoulder'],
+                      ['RightHandThumb1', 'RightHandThumb2', 'RightHandThumb3', 'RightInHandIndex', 'RightHandIndex1', 'RightHandIndex2', 'RightHandIndex3', 'RightInHandMiddle', 'RightHandMiddle1', 'RightHandMiddle2', 'RightHandMiddle3'],
+                      ['RightHand', 'RightArm', 'RightForeArm', 'RightShoulder', 'RightHandThumb1', 'RightHandThumb2', 'RightHandThumb3', 'RightInHandIndex', 'RightHandIndex1', 'RightHandIndex2', 'RightHandIndex3', 'RightInHandMiddle', 'RightHandMiddle1', 'RightHandMiddle2', 'RightHandMiddle3'],
+                      ['Hips'],
+                      ['LeftFoot'],
+                      ['Head'],
+                      ['Spine'],
+                      ['Spine', 'Spine1'],
+                      ['Spine', 'Spine1', 'Spine2'],
+                      ['Spine', 'Spine1', 'Spine2', 'Spine3'],
+                      ['LeftHand'],
+                      ['LeftForeArm'],
+                      ['LeftArm'],
+                      ['LeftShoulder']
 
-    # test_normalization()
-    # test_dtw()
-    # kmeans_algo(new_data)
-    # ml()
-    # res = test_mean_speed_intervals("cut", joints_to_append=['Head', 'LeftShoulder', 'LeftForeArm', 'LeftArm', 'LeftHand'])
-    # pdb.set_trace()
-    # display_res(res)
-    # test_mean_speed_intervals_batch(19, motion_type='cut')
+    ]
 
-    test_full_batch(r'C:\Users\quentin\Documents\Programmation\C++\MLA\Data\Speed\\', joints_to_append=['Hips', 'Spine', 'RightArm'])
+    for joints_to_append in joints_to_test:
+        print(joints_to_append)
+        test_full_batch(r'C:\Users\quentin\Documents\Programmation\C++\MLA\Data\Speed\\', joints_to_append=joints_to_append)
+        print('')
 
+def main_all_joints():
+    test_full_batch_every_joint(r'C:\Users\quentin\Documents\Programmation\C++\MLA\Data\Speed\\')
+    
 if __name__ == '__main__':
-    main()
+    main_all_joints()
 
 
 # [motion["RightHand"], motion["RightForeArm"], motion["RightArm"], motion["RightShoulder"],
