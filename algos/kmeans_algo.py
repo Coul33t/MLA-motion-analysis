49aceb89c6398cb22a445ab32a1b45a41c73e3d7
Coul33t <Coulis1990@gmail.com>
2018-02-01 11:56:08 +0100

    Added k variation as a parameter for k-means algorithm

--- algos/kmeans_algo.py
+++ algos/kmeans_algo.py
@@ -1,11 +1,11 @@
 from sklearn.cluster import KMeans
 
-def kmeans_algo(data, algo='k-means++', verbose=False, extrem_verbose=False):
+def kmeans_algo(data, algo='k-means++', k=2, verbose=False, extrem_verbose=False):
 
     if verbose:
         print("\n\n{}\n".format(algo))
 
     # About fit vs fit_predict: https://stackoverflow.com/questions/25012342/scikit-learns-k-means-what-does-the-predict-method-really-do
-    res = KMeans(2, algo, n_init=20, max_iter=10000, verbose=extrem_verbose).fit(list(data))
+    res = KMeans(k, algo, n_init=20, max_iter=10000, verbose=extrem_verbose).fit(list(data))
 
     return res
