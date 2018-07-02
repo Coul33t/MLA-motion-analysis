41256a1458224bf3c4f3f46ea831ba22c92073ee
Quentin <Coulis1990@gmail.com>
2018-07-02 11:23:52 +0200

    :art: :sparkles: :construction: Further development of recursive clustering

--- recursive_test.py
+++ recursive_test.py
@@ -7,13 +7,15 @@ import networkx as nx
 
 from data_import import json_import
 
-from main import (joint_selection,
-                  data_selection)
+from data_processing import (joint_selection,
+                             data_selection)
 
 from algos.kmeans_algo import kmeans_algo
 
 from algos.metrics import (adjusted_rand_score_computing,
-                           silhouette_score_computing)
+                           silhouette_score_computing,
+                           compute_all_gt_metrics,
+                           compute_all_clustering_metrics)
 
 import data_labels as dl
 
@@ -39,7 +41,18 @@ def motion_classes(original_data, labels):
     return motion_with_classes
 
 def compute_clusters(labels, gt):
+    """
+        This function compute the relative proportion of each class
+        in each cluster.
+        let say we have 100 samples of class +, and 100 samples of
+        class -.
+        c0 : [99+, 99-]
+        c1 : [1+, 1-]
 
+        the result will be
+        c0 : [50%+, 50%-]
+        c1 : [50%+, 50%-]
+    """
     n_labels = labels
     n_gt = [x[1] for x in gt]
 
@@ -48,30 +61,24 @@ def compute_clusters(labels, gt):
     if not isinstance(n_gt, np.ndarray):
         n_gt = np.array(n_gt)
 
-    # We don't know if c0 (labels) == c0 (gt), so we take the labeling
-    # that gives the highest good clustering
+    clusters = []
 
-    score = (len(n_labels) - sum(abs(n_labels - n_gt)))/len(n_labels)
+    for i in range(len(np.unique(n_labels))):
+        clusters.append([])
 
-    # If the best value is for the reversed labels, we do it
-    if (1 - score > score):
-        n_gt = abs(n_gt - 1)
+    for i in range(len(n_gt)):
+        clusters[n_labels[i]].append(n_gt[i])
 
-    idx_gt_0 = np.where(n_gt == 0)
-    idx_labels_0 = np.where(n_labels == 0)
+    res = []
 
-    idx_gt_1 = np.where(n_gt == 1)
-    idx_labels_1 = np.where(n_labels == 1)
+    for cluster in clusters:
+        tmp = []
+        for i in range(len(np.unique(cluster))):
+            tmp.append(cluster.count(i) / len(cluster))
+        res.append(tmp)
 
-    c1_together = 0
-    if len(idx_gt_0[0]) > 0:
-        c1_together = len(np.intersect1d(idx_gt_0, idx_labels_0)) / len(idx_gt_0[0])
+    return res
 
-    c2_together = 0
-    if len(idx_gt_1[0]) > 0:
-        c2_together = len(np.intersect1d(idx_gt_1, idx_labels_1)) / len(idx_gt_1[0])
-
-    return [c1_together, c2_together]
 
 def graph_analysis(g):
     best_nodes = []
@@ -80,6 +87,19 @@ def graph_analysis(g):
         if n['best'] == True:
             best_nodes.append(n)
 
+    for elem in best_nodes:
+        print(f"Data: {elem['data']}")
+        print(f"Joint: {elem['joint']}")
+        print(f"Recall: {elem['recall']}")
+        print(f"Precision: {elem['precision']}")
+        print(f"F score: {elem['f_score']}")
+        print(f"Average Silhouette Score: {elem['ss']}")
+        print(f"Adujsted Rand Score: {elem['ars']}")
+
+def export_node(n):
+    pdb.set_trace()
+
+def graph_text_export(g, filename):
     pdb.set_trace()
 
 def init(c_to_keep=False, debug=False):
@@ -173,18 +193,20 @@ def init_test_stop(c_to_keep=False, debug=False):
             vertex['color'] = 'blue'
         else:
             for k, v in color_dict.items():
-                if v[0] <= max(vertex['c_together']) < v[1]:
+                if v[0] <= max(max(vertex['c_together'])) < v[1]:
                     vertex['color'] = k
 
     bbox = (1000, 1000)
     igraph.plot(i_graph, "output_graph_tree.svg", layout=i_graph.layout('tree'), vertex_size=5, bbox=bbox)
     igraph.plot(i_graph, "output_graph_large.svg", layout=i_graph.layout('large_graph'), vertex_size=5, bbox=bbox)
     igraph.plot(i_graph, "output_graph_circular.svg", layout=i_graph.layout('rt_circular'), vertex_size=5, bbox=bbox)
-
-    pdb.set_trace()
+    igraph.plot(i_graph, "output_graph_tree.png", layout=i_graph.layout('tree'), vertex_size=5, bbox=bbox)
+    igraph.plot(i_graph, "output_graph_large.png", layout=i_graph.layout('large_graph'), vertex_size=5, bbox=bbox)
+    igraph.plot(i_graph, "output_graph_circular.png", layout=i_graph.layout('rt_circular'), vertex_size=5, bbox=bbox)
 
     i_graph.save("output.gml", format="gml")
     i_graph.save("output.graphml", format="graphml")
+    #graph_text_export(i_graph, 'output.txt')
 
     if debug:
         print(f'Iterations: {ITERATION_COUNTER}')
@@ -252,6 +274,17 @@ def recursive_clustering(original_data, data_to_keep, datatype_used, joints, i_g
 
                 if k == 2:
                     value['ars'] = adjusted_rand_score_computing(res.labels_, sub_classes)
+                    dic = compute_all_gt_metrics(res.labels_, [x[1] for x in sub_classes])
+                    for key, val in dic.items():
+                        if key not in value.keys():
+                            value[key] = val
+
+
+                dic = compute_all_clustering_metrics(features, res.labels_)
+                for key, val in dic.items():
+                    if key not in value.keys():
+                        value[key] = val
+
                 # We get the indexes of motion in every clusters
                 for c in range(len(np.unique(res.labels_))):
                     idx_c.append(np.where(res.labels_ == c)[0].tolist())
@@ -303,10 +336,6 @@ def recursive_clustering_test_stop(original_data, original_data_classes, data_to
         Performs a recursive clustering, as long as SS > THRESHOLD
         and sample_n > 2
     """
-
-    # TODO: something without this (because there'll be a huge memory
-    # consumption)
-
     global ITERATION_COUNTER
 
     sub_data = []
@@ -322,26 +351,29 @@ def recursive_clustering_test_stop(original_data, original_data_classes, data_to
             if motion_class[0] in data_to_keep:
                 sub_classes.append(motion_class)
 
-    for data in datatype_used:
-        for joint in joints:
+    max_k_value = 10
 
-            max_k_value = 10
+    if len(sub_data) <= max_k_value:
+        max_k_value = len(sub_data) - 1
 
-            if len(sub_data) <= max_k_value:
-                max_k_value = len(sub_data) - 1
+    print(f"MAX K VALUE {max_k_value}")
+    # No need to compute a k-means for a sample number = 2
+    if max_k_value <= 2:
+        if debug:
+            print(f"MAX_K_VALUE TOO LOW {max_k_value}")
+        return
 
-            # No need to compute a k-means for a sample number = 2
-            if max_k_value <= 2:
-                if debug:
-                    print(f"MAX_K_VALUE TOO LOW {max_k_value}")
-                continue
+    nb_iter = 0
 
-            res_k = []
+    res_k = []
 
-            best_c_together = [0, 0]
+    best_c_together = [0, 0]
 
-            for k in range(2, max_k_value + 1):
+    for k in range(2, max_k_value + 1):
+        for data in datatype_used:
+            for joint in joints:
                 ITERATION_COUNTER += 1
+                nb_iter += 1
                 if debug:
                     print(f'Computing for k={k}, data = {data}, joint = {joint} (p_id = {p_id}, max_k_value = {max_k_value})')
                 # We keep the joints' data we're interested in
@@ -363,9 +395,20 @@ def recursive_clustering_test_stop(original_data, original_data_classes, data_to
                 value = {}
                 value['data'] = data
                 value['joint'] = joint
-                value['ss'] = s_score
+                value['k'] = k
+
+                # GT metrics
                 if k == 2:
-                    value['ars'] = adjusted_rand_score_computing(res.labels_, [x[1] for x in sub_classes])
+                    dic = compute_all_gt_metrics(res.labels_, [x[1] for x in sub_classes])
+                    for key, val in dic.items():
+                        if key not in value:
+                            value[key] = val
+
+                # Clustering (no labels) metrics
+                dic = compute_all_clustering_metrics(features, res.labels_)
+                for key, val in dic.items():
+                    if key not in value.keys():
+                        value[key] = val
 
                 # We get the indexes of motion in every clusters
                 for c in range(len(np.unique(res.labels_))):
@@ -384,16 +427,18 @@ def recursive_clustering_test_stop(original_data, original_data_classes, data_to
                     clustering_res['repartition'][f'c{i}'] = [x.name for i, x in enumerate(original_data) if i in idx_c[0]]
 
                 clustering_res['c_together'] = compute_clusters(res.labels_, sub_classes)
+
                 res_k.append(clustering_res)
 
                 if debug:
                     print(f'c_together: {max(clustering_res["c_together"])}')
+                    print(f'ss: {clustering_res["s_score"]}')
 
                 value['c_together'] = clustering_res['c_together']
                 value['best'] = False
 
-                if max(clustering_res['c_together']) > best_c_together[0]:
-                    best_c_together[0] = max(clustering_res['c_together'])
+                if max(max(clustering_res['c_together'])) > best_c_together[0]:
+                    best_c_together[0] = max(max(clustering_res['c_together']))
                     best_c_together[1] = k - 2
 
                 i_graph.add_vertex(**value)
@@ -402,34 +447,55 @@ def recursive_clustering_test_stop(original_data, original_data_classes, data_to
                 if debug:
                     print(f'Link {p_id[-1]} and {len(i_graph.vs) - 1}')
 
-            s_score_all = [x['s_score'] for x in res_k]
-            to_keep = res_k[s_score_all.index(max(s_score_all))]
+    s_score_all = [x['s_score'] for x in res_k]
+    to_keep = res_k[s_score_all.index(max(s_score_all))]
 
-            i_graph.vs[len(i_graph.vs) - 1 - (max_k_value - 1 - best_c_together[1]) + 1]['best'] = True
+    i_graph.vs[len(i_graph.vs) - 1 - (max_k_value - 1 - best_c_together[1]) + 1]['best'] = True
 
-            p_id.append(len(i_graph.vs) - 1 - (max_k_value - 1 - s_score_all.index(max(s_score_all))) + 1)
+    p_id.append(len(i_graph.vs) - 1 - (max_k_value - 1 - s_score_all.index(max(s_score_all))) + 1)
 
-            if debug:
-                print(f'Max c_together: {max(to_keep["c_together"])}')
-                print(f'new p_id: {p_id}')
+    cluster_to_process = []
+    for cluster_rep in to_keep['c_together']:
+        if max(cluster_rep) < 0.75:
+            cluster_to_process.append(1)
+        else:
+            cluster_to_process.append(0)
 
-            if max(to_keep['c_together']) < 0.75:
-                print(f'c_together too low, continuing...')
-                # Recursive clustering on each cluster
-                for i,rep in enumerate(to_keep['idx_c']):
-                    # If we have less than 3 samples in a cluster,
-                    # no need to perfom clustering on it
-                    if len(rep) < 3:
-                        if debug:
-                            print(f'EMPTY CLUSTER (n°{i})')
-                    else:
-                        if debug:
-                            print(f'CLUSTER SUFFICIENTLY BIG (n°{i})')
-                        names = [x.name for i, x in enumerate(original_data) if i in idx_c[0]]
-                        recursive_clustering_test_stop(original_data, original_data_classes, names, datatype_used, joints, i_graph, p_id, debug)
+    if debug:
+        print(f'Max c_together: {max(to_keep["c_together"])}')
+        print(f'new p_id: {p_id}')
+
+    ##############################################################################
+    #TODO: do clustering for all sub_clusters that don't have the threshold value#
+    ##############################################################################
+
+    if cluster_to_process:
+        print(f'c_together too low, continuing...')
+        # Recursive clustering on each cluster
+        for i,rep in enumerate(to_keep['idx_c']):
+
+            if cluster_to_process[i] == 0:
+                if debug:
+                    print(f'NO NEED TO PROCESS CLUSTER {i} (REP > 0.75)')
+                continue
+
+            # If we have less than 3 samples in a cluster,
+            # no need to perfom clustering on it
+            if len(rep) < 3:
+                if debug:
+                    print(f'EMPTY CLUSTER (n°{i})')
+                continue
+            else:
+                if debug:
+                    print(f'CLUSTER SUFFICIENTLY BIG (n°{i})')
+                print(f'CLUSTER SUFFICIENTLY BIG (n°{i})')
+                print(f'(max ss = {to_keep["s_score"]}')
+                print(f'(all s_score: {s_score_all}')
+                names = [x.name for i, x in enumerate(original_data) if i in idx_c[0]]
+                recursive_clustering_test_stop(original_data, original_data_classes, names, datatype_used, joints, i_graph, p_id, debug)
 
 
-                p_id.pop()
+    p_id.pop()
 
 
     if debug:
@@ -438,4 +504,4 @@ def recursive_clustering_test_stop(original_data, original_data_classes, data_to
 
 if __name__ == '__main__':
     #init(c_to_keep=0, debug=True)
-    init_test_stop(debug=True)
+    init_test_stop(debug=False)
